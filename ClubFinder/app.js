35c0a22433ee4870968e0993f7aaf9911a92e7da
Erza Putra Albasori <erzaputra9110@gmail.com>
2020-11-29 01:16:35 +0700

    add: webpack in club finder

--- ClubFinder/app.js
+++
@@ -1,7 +0,0 @@
-import main from './src/script/view/main.js';
-
-import './src/script/component/AppBar.js';
-import './src/script/component/SearchBar.js';
-import './src/script/component/ClubList.js';
-
-document.addEventListener("DOMContentLoaded", main);
---
+++ ClubFinder/src/app.js
@@ -0,0 +1,9 @@
+import main from './script/view/main.js';
+
+import './styles/style.css';
+
+import './script/component/AppBar.js';
+import './script/component/SearchBar.js';
+import './script/component/ClubList.js';
+
+document.addEventListener("DOMContentLoaded", main);
